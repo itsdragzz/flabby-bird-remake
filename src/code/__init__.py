d49ed43dfecaa830c4f5dd30dc0a650a05c5f112
Kelvin <Kelvinliang123@icloud.com>
2022-10-11 19:50:37 +1100

    idk

--- src/code/__init__.py
+++ src/code/__init__.py
@@ -2,3 +2,4 @@ from code.settings import Settings
 from code.blocks import Blocks
 from code.player import Player
 from code.renderer import Display
+from code.physics import Falling
--- src/code/player.py
+++ src/code/player.py
@@ -1,28 +1,25 @@
 import pygame
 import code
 
-class Player():
+class Player(pygame.sprite.Sprite):
     """
     Create a player
     """
 
     def __init__(self, screen):
+        super().__init__() 
+
         # pygame.sprite.Sprite.__init__(self)
 
 
         self.image = pygame.image.load("src/graphics/bird.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (1280, 720))
+        self.image = pygame.transform.scale(self.image, (150, 100))
         self.rect = self.image.get_rect(topleft = (0, 0))
 
 
         code.Display(screen, self.image, self.rect)
 
+
+        code.Falling(self.rect)
+
             
-
-        def x(self):
-
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        pass
-    
